210d0aa3db2d571e0e8b886cf27a5d1d8d40efe9
santiaago <santiago.ariassar@gmail.com>
2013-10-21 16:38:31 +0200

    hello

---
+++ gs_ex1.cpp
@@ -0,0 +1,10 @@
+//  gs_ex1.cpp: Santiago Arias
+// Description: a program that prints the immortal saying "hello world"
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello World!" << endl;
+  return 0;
+}
