376de812add45935376b0c23da735df8efba16a0
santiaago <santiago.ariassar@gmail.com>
2013-10-21 17:08:11 +0200

    hello with space left

--- gs_ex1.cpp
+++ gs_ex1.cpp
@@ -8,9 +8,11 @@ using namespace std;
 
 int main(){
 
-  
+  // set up cout to right-align. if not set default is left align
+  cout<< setiosflags(ios::left);
   for(int i = 0; i < 6; i++){
     for(int j = 0; j < 4; j++){
+      // setw(int) sets the column width
       cout << setw(17);
       cout << "Hello World!";
     }
