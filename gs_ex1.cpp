64a4445ebe12f883e35b968aafd5db354597d8a1
santiaago <santiago.ariassar@gmail.com>
2013-10-21 16:59:57 +0200

    hello with space right

--- gs_ex1.cpp
+++ gs_ex1.cpp
@@ -2,9 +2,19 @@
 // Description: a program that prints the immortal saying "hello world"
 
 #include <iostream>
+#include <iomanip> // needed for setw
+
 using namespace std;
 
 int main(){
-  cout << "Hello World!" << endl;
+
+  
+  for(int i = 0; i < 6; i++){
+    for(int j = 0; j < 4; j++){
+      cout << setw(17);
+      cout << "Hello World!";
+    }
+    cout<< endl;
+  }
   return 0;
 }
