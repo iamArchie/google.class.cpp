5ab6e8f1f248b1a4d304db8a5a732ccb2ca60ba3
santiaago <santiago.ariassar@gmail.com>
2013-10-25 14:15:47 +0200

    multiple records

--- gs_ex9.cpp
+++ gs_ex9.cpp
@@ -8,25 +8,39 @@ using namespace std;
 int main(){
   char first_name[30], last_name[30]; int age;
   char file_name[20];
+  char done;
   // Collect the data.
-  cout << "Enter First Name: "; cin >> first_name;
-  cout << "Enter Last Name: "; cin >> last_name;
-  cout << "Enter Age: "; cin >> age;
   cout << endl << "Enter the name of the file: "; cin >> file_name;
 
   // Create an ofstream called People, open the stream for output.
   ofstream People(file_name, ios::out);
   // Write the output to the stream.
-  People << first_name << endl << last_name << endl << age << endl;
 
-  // Read
+  while(1){
+    cout << "Enter First Name: "; cin >> first_name;
+    cout << "Enter Last Name: "; cin >> last_name;
+    cout << "Enter Age: "; cin >> age;
+    People << first_name << endl << last_name << endl << age << endl;
+    cout << "Continue? (y/n)"<< endl;
+    cin >> done;
+    if(done == 'n'){
+      break;
+    }
+  }
+  People.close();
+
   ifstream People_in (file_name, ios::in);
-  People_in >> first_name >> last_name >> age;
+  while(1){
+    People_in >> first_name >> last_name >> age;
 
-  cout << endl << "Enter First Name: " << first_name;
-  cout << endl << "Enter Last Name: " << last_name;
-  cout << endl << "Enter Age: " << age;
-  cout << endl;
+    if(People_in.eof()){
+      break;
+    }
+    cout << endl << "Enter First Name: " << first_name;
+    cout << endl << "Enter Last Name: " << last_name;
+    cout << endl << "Enter Age: " << age;
+    cout << endl;
+  }
 
   People_in.close();
   return 0;
