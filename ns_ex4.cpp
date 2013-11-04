b7390505c1c8c9668b113b1f9218fc9196b7f746
santiaago <santiago.ariassar@gmail.com>
2013-11-04 23:37:39 +0100

    next step ex 4 timer class

---
+++ ns_ex4.cpp
@@ -0,0 +1,47 @@
+// ns_ex4.cpp: Santiago Arias
+// Description: classes and objects
+
+#include <iostream>
+
+using namespace std;
+
+// a simple time class
+class Time{
+private:
+  int hours_;
+  int minutes_;
+  int seconds_;
+public:
+  void set(int h, int m, int s){hours_ = h; minutes_ = m; seconds_ = s;}
+  void increment(void);
+  void display(void);
+};
+
+void Time::increment(void){
+  seconds_++;
+  minutes_ += seconds_/60;
+  hours_ += minutes_/60;
+  seconds_ %= 60;
+  minutes_ %= 60;
+  hours_ %= 24;
+  return;
+}
+
+void Time::display(void){
+
+  cout << (hours_ % 12? hours_%12:12) << ":" <<
+    (minutes_ < 10 ? "0" : "" ) << minutes_ << ":" <<
+    (seconds_ < 10 ? "0" : "") << seconds_ << 
+    (hours_ < 12 ? " AM" : " PM") << endl;
+}
+
+int main(){
+
+  Time timer; timer.set(23,59,58);
+  for(int i = 0; i < 5; i++){
+    timer.increment();
+    timer.display();
+    cout << endl;
+  }
+
+}
