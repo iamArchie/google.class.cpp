7ecfbfedf56bc77f2a4982d93a6c0662dba28026
santiaago <santiago.ariassar@gmail.com>
2013-11-08 23:30:49 +0100

    clean up, need refactor

--- ns_warm_up2.cpp
+++ ns_warm_up2.cpp
@@ -1,52 +1,11 @@
 // ns_warm_up2.cpp: Santiago Arias
-// Description: 
-
-// Your college book store needs your help in estimating its business for next year. 
-// Experience has shown that sales depend greatly on whether a book is required for a 
-// course or just optional, and whether or not it has been used in the class before. 
-// A new, required textbook will sell to 90% of prospective enrollment, but if it has 
-// been used in the class before, only 65% will buy. 
-// Similarly, 40% of prospective enrollment will buy a new, optional textbook, but if 
-// it has been used in the class before only 20% will buy. 
-// (Note that "used" here does not mean second-hand books.)
-
-// Write a program that accepts as input a series of books (until the user enters a sentinel).
-
-// For each book ask for: 
-//   - a code for the book, 
-//   - the single copy cost for the book, 
-//   - the current number of books on hand, 
-//   - the prospective class enrollment, 
-//   - data that indicates if the book is required/optional
-//   - new/used in past. 
-
-// As output, 
-//   - show all the input information in a nicely formatted screen 
-//   - how many books must be ordered (if any, note that only new books are ordered),
-//   - the total cost of each orders.
-
-
-// Then, after all input is complete.
-
-// show:
-//     - the total cost of all book orders
-//     - the expected profit if the store pays 80% of list price
-//     - process one book at a time
-//     - show the output screen for that book.
- 
-// Then, when the user has finished entering all the data.
-//   your program should output:
-//   - the total
-//   - profit values.
-
-// Before you start writing code, take some time to think about design of this program. 
-// Decompose into a set of functions, and create a main() function that reads like an outline 
-// for your solution to the problem. Make sure each function does one task.
+// Description: https://developers.google.com/edu/c++/next-steps
 
 #include <iostream>
 using namespace std;
 
 int main(){
+
   int book_code;
   double single_copy_price;
   int number_on_hand;
@@ -153,13 +112,4 @@ int main(){
   cout << "Total for all orders: " << total_orders << endl;
   cout << "Profit: " << profit << endl;
   cout << "**************************************************" << endl << endl;
-
-  // show:
-  // total cost of books ordered
-  // expected profit it the store pays 80% of list price
-  // process one book at a time
-  // show the output screen for that book.
-
-  //output total
-  // output profil values.
 }
