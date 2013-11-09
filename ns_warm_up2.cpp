e8710aef57bcec47c2d848c29be209b555c9b057
santiaago <santiago.ariassar@gmail.com>
2013-11-09 17:08:07 +0100

    create book class

--- ns_warm_up2.cpp
+++ ns_warm_up2.cpp
@@ -4,29 +4,64 @@
 #include <iostream>
 using namespace std;
 
+// Book class:
+class Book{
+private:
+  int code_;
+  double price_;
+  string str_mode_;
+  string str_age_;
+  bool is_new_;
+  bool is_required_;  
+  double percent_sold_;
+
+  double compute_percent_sold(){
+    if( is_required_ && is_new_ ){
+      return 0.9;
+    } else if( is_required_ && !is_new_ ){
+      return 0.65;
+    } else if( !is_required_ && is_new_ ){
+      return 0.4;
+    } else if( !is_required_ && !is_new_ ){
+      return 0.2;
+    }
+    
+  }
+public:
+  Book(int code, double price, int mode, int age ):
+    code_(code), price_(price), percent_sold_(0) {
+
+    is_new_ = (age == 1)? true : false;
+    is_required_ = (mode == 1)? true : false;
+    str_age_ = is_new_? "new" : "used";
+    str_mode_ = is_required_? "required" : "optional";
+    percent_sold_ = compute_percent_sold();
+
+  }
+
+  double get_precent_sold(){ return percent_sold_;}
+  double get_price(){return price_;}
+  int get_code(){return code_;}
+  string get_mode(){return str_mode_;}
+  string get_age(){return str_age_;}
+};
+
 int main(){
 
-  int book_code;
+  int book_code(0);
   double single_copy_price;
-  int number_on_hand;
-  int prospective_enrollment;
-  
+  int number_on_hand(0);
+  int prospective_enrollment(0);
   int book_mode;
-  string str_book_mode;
-  bool book_required = false;
-  bool book_optional = false;
-
   int book_age;
-  string str_book_age;
-  bool book_new = false;
-  bool book_used = false;
-
+  
   double precent_sold;
-  int number_books_to_order;
+  int number_books_to_order(0);
   int another_book(0);
   double total_cost(0);
   double total_orders(0);
   double profit(0);
+
   do{
     total_orders += total_cost;
     total_cost = 0;
@@ -55,45 +90,23 @@ int main(){
     if(!(cin >> book_mode)){
       continue;
     }
-    if(book_mode == 1) {
-      book_required = true;
-      str_book_mode = "required";
-    }else{
-      book_optional = true;
-      str_book_mode = "optional";
-    }
 
     cout << "1 for new/0 for used: 0: ";
     if(!(cin >> book_age)){
       continue;
     }
-    if(book_age == 1) {
-      book_new = true;
-      str_book_age = "new";
-    }else{
-      book_used = true;
-      str_book_age = "used";
-    }
 
-    if( book_required && book_new ){
-      precent_sold = 0.9;
-    } else if( book_required && book_used ){
-      precent_sold = 0.65;
-    } else if( book_optional && book_new ){
-      precent_sold = 0.4;
-    } else if( book_optional && book_used ){
-      precent_sold = 0.2;
-    }
-    number_books_to_order = (prospective_enrollment* precent_sold) - number_on_hand;
-    total_cost =  number_books_to_order * single_copy_price;
+    Book book(book_code, single_copy_price, book_mode, book_age);
+    number_books_to_order = (prospective_enrollment* book.get_precent_sold()) - number_on_hand;
+    total_cost =  number_books_to_order * book.get_price();
 
     cout << "**************************************************" << endl;
-    cout << "Book: " << book_code << endl;
-    cout << "Price: " <<  single_copy_price << endl;
+    cout << "Book: " << book.get_code() << endl;
+    cout << "Price: " <<  book.get_price() << endl;
     cout << "Inventory: " << number_on_hand << endl;
     cout << "Enrollement: " << prospective_enrollment << endl << endl;
     
-    cout << "This book is " << str_book_mode <<  " and " << str_book_age << "." << endl;
+    cout << "This book is " << book.get_mode() <<  " and " << book.get_age() << "." << endl;
     cout << "**************************************************" << endl;
     cout << "Need to order: " << number_books_to_order << endl; 
     cout << "Total Cost: " << total_cost << endl;
